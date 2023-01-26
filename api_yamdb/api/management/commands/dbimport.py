e13224f32375da39e7e69ef4bfb27c66b6283410
Вадим Шпак <starboyshpak@yandex.ru>
2023-01-26 23:48:55 +0300

    fix pep8

--- api_yamdb/api/management/commands/dbimport.py
+++ api_yamdb/api/management/commands/dbimport.py
@@ -1,6 +1,7 @@
-from django.core.management.base import BaseCommand
-from django.apps import apps
 import csv
+
+from django.apps import apps
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
